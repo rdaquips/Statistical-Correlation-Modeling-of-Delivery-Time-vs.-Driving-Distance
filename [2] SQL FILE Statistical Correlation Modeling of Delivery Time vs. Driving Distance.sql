01f807f36e7939cf3e995d272faf71c80b49eef4
Rafael Daquipil <rafael.daquipil08@gmail.com>
2026-08-05 17:14:24 +0800

    Update [2] SQL FILE Statistical Correlation Modeling of Delivery Time vs. Driving Distance.sql

--- [2] SQL FILE Statistical Correlation Modeling of Delivery Time vs. Driving Distance.sql
+++ [2] SQL FILE Statistical Correlation Modeling of Delivery Time vs. Driving Distance.sql
@@ -63,17 +63,14 @@ WITH IS_SQL AS (
         END AS stage_0,
         CASE 
             WHEN (order_status_label = 'Completed' OR delivery_performance = 'Untagged') 
-            AND order_ready_for_pickup_time IS NOT NULL 
             THEN 1 ELSE 0 
         END AS stage_1,
         CASE 
             WHEN (order_status_label = 'Completed' OR delivery_performance = 'Untagged') 
-            AND order_pickup_done_time IS NOT NULL 
             THEN 1 ELSE 0 
         END AS stage_2,
         CASE 
-            WHEN (order_status_label = 'Completed' OR delivery_performance = 'Untagged') 
-            AND order_delivered_time IS NOT NULL 
+            WHEN (order_status_label = 'Completed' OR delivery_performance = 'Untagged')  
             THEN 1 ELSE 0 
         END AS stage_3
     FROM IS_SQL
